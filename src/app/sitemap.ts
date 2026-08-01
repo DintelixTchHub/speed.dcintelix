a2171e38a312f80636d219a4d28110e7be29c27a
dushimec <dushimec515@gmail.com>
2026-08-01 01:30:49 -0700

    fixed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,12 @@
-const pages = [
-  {
-    url: "https://speedtest.dcintelix.rw",
-    lastmod: new Date().toISOString(),
-    changefreq: "daily",
-    priority: 1,
-  },
-];
+import type { MetadataRoute } from 'next'
 
-export default pages;
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://speedtest.dcintelix.rw',
+      lastmod: new Date().toISOString(),
+      changefreq: 'daily',
+      priority: 1,
+    },
+  ];
+}
